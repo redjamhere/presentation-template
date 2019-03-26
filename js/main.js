418599806ad8abecb4d03f134deca006e58aeb8f
redjamhere <aynur.zaymanov@mail.ru>
2019-03-26 10:37:55 +0300

    rep init

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+	$('#fullpage').fullpage({
+		//options here
+		autoScrolling:true,
+    scrollHorizontally: true,
+    //design
+    controlArrows: true,
+		verticalCentered: false,
+		controlArrows: false,
+	});
+
+	//methods
+});
+
+new WOW().init()
+
